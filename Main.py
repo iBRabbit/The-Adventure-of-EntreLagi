e4606c8e0c520c59aa1736bbae3aa88c9a730ff7
Felix Prima <felix.prima@binus.ac.id>
2021-12-05 01:40:12 +0700

    Basic Move

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import turtle
 import time
-import os
+import random
 
-delay = 0.001
+DELAY = 0.001
+MAX_OBS = 10
+MAX_ENEMIES = 2
 
 window = turtle.Screen()
 window.title("Pac-Entre-Lagi")
@@ -13,40 +15,95 @@ window.tracer(0)
 player = turtle.Turtle()
 player.speed(0)
 player.shape("square")
-player.color("lightgreen")
+player.color("aqua")
 player.penup()
 player.goto(0,0)
 player.direction = "stop"
 
+obs = [] # Obstacles
+enemies = [] # Enemies
+
 # Functions #
 
-def move_up():
+def initObstacles():
+    for i in range(MAX_OBS):
+        posx = random.randint(-14, 14) * 20 
+        posy = random.randint(-14, 14) * 20
+        
+        ob = turtle.Turtle()
+        ob.speed(0)
+        ob.goto(posx, posy)
+        ob.penup
+        ob.color("green")
+        ob.shape("triangle")
+        obs.append(ob)
+
+def initEnemies():
+    for i in range(MAX_ENEMIES):
+        posx = random.randint(-14, 14) * 20 
+        posy = random.randint(-14, 14) * 20
+        
+        en = turtle.Turtle()
+        en.speed(0)
+        en.goto(posx, posy)
+        en.penup
+        en.color("red")
+        en.shape("circle")
+        enemies.append(en)
+
+def getPlayerCurrentPos():
+    curr_x = player.xcor()
+    curr_y = player.ycor()
+    print("[DEBUG] : Player Current Pos -> ", player.pos())
+    return curr_x, curr_y
+
+def moveUp():
+    getPlayerCurrentPos()
+    x = player.xcor()
     y = player.ycor()
+    if obstaclesCheck(x, y + 20) == False : return False;
     player.sety(y + 20)
 
-def move_down():
+def moveDown():
+    getPlayerCurrentPos()
+    x = player.xcor()
     y = player.ycor()
+    if obstaclesCheck(x, y - 20) == False : return False;
     player.sety(y - 20)
 
-def move_left():
-    x = player.xcor()
+def moveLeft():
+    getPlayerCurrentPos()
+    x = player.xcor()   
+    y = player.ycor()
+    if obstaclesCheck(x - 20, y) == False : return False;
     player.setx(x - 20)
 
-def move_right():
+def moveRight():
+    getPlayerCurrentPos()
     x = player.xcor()
+    y = player.ycor()
+    if obstaclesCheck(x + 20, y) == False : return False;
     player.setx(x + 20)
 
-
-# Keyboard 
-window.listen()
-window.onkey(move_up, "w")
-window.onkey(move_down, "s")
-window.onkey(move_left, "a")
-window.onkey(move_right, "d")
-
-while True:
-    window.update()
-    time.sleep(delay)
+def obstaclesCheck(nextX, nextY):
+    for i in range(MAX_OBS):
+        if nextX == obs[i].xcor() and nextY == obs[i].ycor() : return False
     
-window.mainloop()
-    
+    return True
+
+def Main():
+    initObstacles()
+    initEnemies()
+    window.listen()
+    window.onkey(moveUp, "w")
+    window.onkey(moveDown, "s")
+    window.onkey(moveLeft, "a")
+    window.onkey(moveRight, "d")
+
+    while True:
+        window.update()
+        time.sleep(DELAY)
+        
+    window.mainloop()
+
+Main()
